488714b7af3a3e544bbff966dd2c1442547b05ac
planetarianZero <m18333586810@163.com>
2017-11-23 16:35:45 +0800

    Update IntegerDemo.java

--- JavaSE/day02/IntegerDemo.java
+++ JavaSE/day02/IntegerDemo.java
@@ -4,6 +4,9 @@ package day02;
  * Java对于8个基本类型分别定义了对应的包装类，出现原因是解决不能面向对象开发的问题
  * 数字类型的包装类继承自Number
  * 提供了如intValue，doubleValue等方法，作用是将当前包装类的数字以其他数字类型的形式返回
+ * 较常用方法
+ * 包装类.parseXXX(String str)
+ * 将字符串转换为包装类相应的基本类型。
  * @author planetarianZero
  *
  */
